e1ac114f72b21c7d7163d72ea6387fb78adb3d81
José Falsarella <joseh.oliveira0329@gmail.com>
2024-09-11 15:08:56 -0300

    add Thumbnails table

---
+++ lib/cuidarMePrj/thumbnail.ex
@@ -0,0 +1,24 @@
+defmodule CuidarMePrj.Thumbnail do
+  use Ecto.Schema
+  import Ecto.Changeset
+
+  schema "thumbnails" do
+    field :url, :string
+    field :file_name, :string
+    field :content_url, :string
+    field :mapped_content_url, :string
+    field :content_type, :string
+    field :size, :integer
+    field :width, :integer
+    field :height, :integer
+    field :inline, :boolean
+    field :deleted, :boolean
+  end
+
+  @doc false
+  def changeset(thumbnail, attrs) do
+    thumbnail
+    |> cast(attrs, [])
+    |> validate_required([])
+  end
+end
---
+++ priv/repo/migrations/20240911174922_create_thumbnails.exs
@@ -0,0 +1,22 @@
+defmodule CuidarMePrj.Repo.Migrations.CreateThumbnails do
+  use Ecto.Migration
+
+  def change do
+    create table(:thumbnails) do
+      add :url, :string
+      add :file_name, :string
+      add :content_url, :string
+      add :mapped_content_url, :string
+      add :content_type, :string
+      add :size, :integer
+      add :width, :integer
+      add :height, :integer
+      add :inline, :boolean
+      add :deleted, :boolean
+    end
+
+    create unique_index(:thumbnails, [:url])
+    create unique_index(:thumbnails, [:content_url])
+    create unique_index(:thumbnails, [:mapped_content_url])
+  end
+end
