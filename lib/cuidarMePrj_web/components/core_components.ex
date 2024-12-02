19d88509055cea180177c548b148da48594866f2
José Falsarella <joseh.oliveira0329@gmail.com>
2024-12-02 16:56:27 -0300

    feat individual ticket data template

--- lib/cuidarMePrj_web/components/core_components.ex
+++ lib/cuidarMePrj_web/components/core_components.ex
@@ -94,6 +94,61 @@ defmodule CuidarMePrjWeb.CoreComponents do
     """
   end
 
+  attr :id, :string, required: true
+  attr :show, :boolean, default: false
+  attr :on_cancel, JS, default: %JS{}
+  slot :inner_block, required: true
+  def modal2(assigns) do
+    ~H"""
+    <div
+      id={@id}
+      phx-mounted={@show && show_modal(@id)}
+      phx-remove={hide_modal(@id)}
+      data-cancel={JS.exec(@on_cancel, "phx-remove")}
+      class="relative z-50 hidden"
+    >
+      <div id={"#{@id}-bg"} class="bg-zinc-900 fixed inset-0 transition-opacity" aria-hidden="true" />
+      <div
+        class="fixed inset-0 overflow-y-auto"
+        aria-labelledby={"#{@id}-title"}
+        aria-describedby={"#{@id}-description"}
+        role="dialog"
+        aria-modal="true"
+        tabindex="0"
+      >
+        <div class="flex min-h-full  items-center justify-center">
+          <div class=" p-4 sm:p-6 lg:py-8">
+            <.focus_wrap
+              id={"#{@id}-container"}
+              phx-window-keydown={JS.exec("data-cancel", to: "##{@id}")}
+              phx-key="escape"
+              phx-click-away={JS.exec("data-cancel", to: "##{@id}")}
+              class="shadow-zinc-700/10 ring-zinc-700/10 w-[1100px] h-auto relative hidden rounded-2xl bg-white p-6 shadow-lg ring-1 transition"
+            >
+              <div class="absolute top-6 right-5">
+                <button
+                  phx-click={JS.exec("data-cancel", to: "##{@id}")}
+                  type="button"
+                  class="-m-4 flex-none p-5"
+                  aria-label={gettext("close")}
+                >
+                  <.icon name="hero-x-mark" class="h-6 w-6 text-darkBlue"/>
+                </button>
+              </div>
+              <div id={"#{@id}-content"}>
+                <%= render_slot(@inner_block) %>
+                <div class="ml-7 my-8">
+
+                </div>
+              </div>
+            </.focus_wrap>
+          </div>
+        </div>
+      </div>
+    </div>
+    """
+  end
+
   @doc """
   Renders flash notices.
 
--- lib/cuidarMePrj_web/live/id_live.ex
+++ lib/cuidarMePrj_web/live/id_live.ex
@@ -1,4 +1,37 @@
 defmodule CuidarMePrjWeb.IdLive do
   use CuidarMePrjWeb, :live_view
+  import CuidarMePrjWeb.GetDataHelper
+
+  def mount(%{"id" => ticket_id}, _session, socket) do
+    ticket = get_ticket(ticket_id)
+    ticket_attributes = load_ticket_attributes(ticket)
+    requester_attributes = get_requester(ticket.id)
+    assignee_attributes = get_assignee(ticket.id)
+    submitter_attributes = get_submitter(ticket.id)
+    comments = get_comments(ticket.id)
+
+    socket = assign(socket,
+      ticket: ticket,
+      ticket_attributes: ticket_attributes,
+      requester_attributes: requester_attributes,
+      assignee_attributes: assignee_attributes,
+      submitter_attributes: submitter_attributes,
+      comments: comments,
+      selected_comment: nil,
+      show_comments: false)
+    {:ok, socket}
+  end
+
+  def handle_event("toggle_comments", _params, socket) do
+    socket = Phoenix.Component.update(socket, :show_comments, fn show_comments -> !show_comments end)
+    {:noreply, socket}
+  end
+
+  def handle_event("show_modal", %{"comment-id" => comment_id}, socket) do
+    selected_comment = Enum.find(socket.assigns.comments, fn comment -> comment.id == String.to_integer(comment_id) end)
+    socket = assign(socket, :selected_comment, selected_comment)
+    {:noreply, socket}
+  end
+
 
 end
